565264511d8bb2e7e3de5f6a7d93b8c0949ed455
Siddharth Motwani <89924392+SiddharthMotwani08@users.noreply.github.com>
2024-05-28 14:12:35 +0530

    Update LandingPage.jsx

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -10,16 +10,16 @@ function LandingPage() {
       data-scroll-speed="-.3"
       className='w-full h-screen bg-zinc-900 pt-1'
     >
-      <div className='textstructure mt-52 uppercase px-10 md:px-20 text-4xl md:text-8xl font-sans font-bold leading-[11vh] tracking-tighter'>
+      <div className='textstructure mt-52 uppercase px-10 md:px-14 text-4xl md:text-8xl font-sans font-bold tracking-tighter'>
         {['We Create', 'Eye Opening', 'presentations'].map((item, index) => (
           <div key={index}>
-            <div className='flex w-fit items-baseline overflow-hidden'>
+            <div className='flex w-full items-baseline'>
               {index === 1 && (
                 <motion.div
                   initial={{ width: 0 }}
                   animate={{ width: '7.5vw' }}
                   transition={{ ease: [0.76, 0, 0.24, 1], duration: 1 }}
-                  className='w-[7.5vw] h-[4.5vw] bg-red-500 rounded-md relative ml-2 mr-2'
+                  className='w-[7.5vw] h-[4.5vw] bg-red-500 rounded-md relative ml-1 mr-1'
                 ></motion.div>
               )}
               <h1 className='masker'>{item}</h1>
@@ -34,7 +34,7 @@ function LandingPage() {
         <p className='md:ml-4'>
           From the first pitch to IPO
         </p>
-        <div className='flex gap-1 mt-4 md:mt-0'>
+        <div className='flex gap-1 mt-[1vw] md:mt-0'>
           <div className='uppercase px-3 py-1 border-2 rounded-full'>
             start the project
           </div>
