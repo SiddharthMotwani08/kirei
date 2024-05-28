b7af12920179dad1c69d3289d8097ea2c65a60ed
Siddharth Motwani <89924392+SiddharthMotwani08@users.noreply.github.com>
2024-05-28 14:14:31 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <div className='fixed z-[999] w-full px-5 md:px-20 flex flex-col md:flex-row justify-between items-center'>
-      <div className='logo w-full md:w-1/12 mb-4 md:mb-0'>
+    <div className='fixed z-[999] w-full px-5 md:px-10 flex flex-col md:flex-row justify-between items-center'>
+      <div className='logo w-1/6 md:w-[10vw] mb-4 md:mb-0'>
         <img src="https://i.ibb.co/sWrK8Zf/A-logo-for-a-website-design-company-named-Kirei.png" alt="Kirei logo" />
       </div>
-      <div className='links gap-4 md:gap-8 flex flex-wrap'>
+      <div className='links gap-4 md:gap-6 flex'>
         {["Services", "Our work", "About us", "Insights", "Contact us"].map((item, index) => (
           <button key={index} className={`text-lg font-mono ${index === 4 ? "ml-0 md:ml-4" : ""}`}>
             {item}
