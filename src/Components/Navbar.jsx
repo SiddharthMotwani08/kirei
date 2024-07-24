5eec3d3596499400d9cb07fcad9a70d32ed68205
Siddharth Motwani <89924392+SiddharthMotwani08@users.noreply.github.com>
2024-07-24 17:05:36 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -39,14 +39,14 @@ function Navbar() {
   
   return (
     <div ref={navbarRef}
-         className='fixed w-full z-[999] px-5 md:px-10 py-[0.1px] flex flex-col md:flex-row justify-between items-stretch'
+         className='fixed w-full z-[999] px-5 md:px-8 flex justify-between items-stretch'
     >
       <div className='logo w-1/6 md:w-[8vw] md:mb-0'>
-        <img src="https://i.ibb.co/sWrK8Zf/A-logo-for-a-website-design-company-named-Kirei.png" alt="Kirei logo" />
+        <img src="https://i.ibb.co/sWrK8Zf/A-logo-for-a-website-design-company-named-Kirei.png" />
       </div>
       <div className='links gap-4 md:gap-6 flex'>
         {["Services", "Our work", "About us", "Insights", "Contact us"].map((item, index) => (
-          <button key={index} className={`text-lg font-mono ${index === 4 ? "ml-10 md:ml-16" : ""}`}>
+          <button key={index} className={`text-sm md:text-lg font-mono ${index === 4 ? "ml-8 md:ml-16" : ""}`}>
             {item}
           </button>
         ))}
