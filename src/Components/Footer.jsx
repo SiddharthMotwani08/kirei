b19909524e2750376af1ba455417ee738a9c4f70
Siddharth Motwani <89924392+SiddharthMotwani08@users.noreply.github.com>
2024-07-24 17:05:04 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,29 +5,29 @@ function Footer() {
     <div
       data-scroll
       data-scroll-speed=".2"
-      className='w-full h-screen p-8 md:p-16 flex md:flex-row'
+      className='w-full h-screen p-8 md:p-16 flex'
     >
-      <div className='w-full md:w-1/2 h-full flex flex-col justify-between'>
+      <div className='w-1/2 h-full flex flex-col justify-between items-start overflow-hidden'>
         <div>
-          <h1 className='text-4xl md:text-[10vh] font-semibold -mb-4 leading-none tracking-tighter'>
+          <h1 className='text-4xl md:text-[8vh] font-semibold md:-mb-2 leading-none tracking-tighter'>
             EYE-
           </h1>
-          <h1 className='text-4xl md:text-[10vh] font-semibold -mb-4 leading-none tracking-tighter'>
+          <h1 className='text-4xl md:text-[8vh] font-semibold -mb-4 leading-none tracking-tighter'>
             OPENING
           </h1>
         </div>
-        <div className='w-32'>
+        <div className='w-40'>
           <img
             src="https://i.ibb.co/sWrK8Zf/A-logo-for-a-website-design-company-named-Kirei.png"
             alt="Kirei logo"
           />
         </div>
       </div>
-      <div className='w-full md:w-1/2'>
-        <h1 className='text-4xl md:text-[10vh] font-semibold leading-none tracking-tighter'>
-          PRESENTATIONS
+      <div className='w-1/2 h-full flex flex-col justify-between'>
+        <h1 className='text-4xl md:text-[8vh] font-semibold leading-none tracking-tighter hyphens-manual'>
+          PRESENTATIO&shy;NS
         </h1>
-        <div className='mt-8 md:mt-60'>
+        <div>
           <a className='block text-lg md:text-xl font-extralight mb-2' href="">
             Facebook
           </a>
