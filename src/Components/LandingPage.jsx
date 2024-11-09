ba14d94499c1868179f08cdd6745ec1b47742604
Siddharth Motwani <89924392+SiddharthMotwani08@users.noreply.github.com>
2024-11-09 12:20:35 +0530

    Update LandingPage.jsx

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -10,7 +10,7 @@ function LandingPage() {
       data-scroll-speed="-.05"
       className='w-full h-screen bg-zinc-900 pt-1'
     >
-      <div className='textstructure mt-52 uppercase px-10 md:px-14 text-6xl md:text-8xl font-sans font-bold tracking-tighter'>
+      <div className='textstructure mt-52 uppercase px-10 md:px-14 text-5xl md:text-8xl font-sans font-bold tracking-tighter'>
         {['We Create', 'Eye Opening', 'presentations'].map((item, index) => (
           <div key={index}>
             <div className='flex w-full items-baseline'>
@@ -19,7 +19,7 @@ function LandingPage() {
                   initial={{ width: 0 }}
                   animate={{ width: '7.5vw'}}
                   transition={{ ease: [0.76, 0, 0.24, 1], duration: 1 }}
-                  className='bg-red-500 w-[7.5vw] h-[6vh] md:h-[9.3vh] rounded-md relative ml-1 mr-1'
+                  className='bg-red-500 w-[7.5vw] h-[34.5px] md:h-[68px] rounded-md relative ml-1 mr-1'
                 ></motion.div>
               )}
               <h1 className='masker'>{item}</h1>
@@ -28,17 +28,17 @@ function LandingPage() {
         ))}
       </div>
       <div className='mt-24 md:mt-36 border-zinc-800 border-t-2 flex flex-col gap-4 md:flex-row justify-between items-center px-10 md:px-20 py-4'>
-        <p className='md:mr-4 mt-10 md:mt-0 '>
+        <p className='md:mr-4 mt-10 md:mt-0 text-md md:text-sm'>
           For public and private companies
         </p>
-        <p className='md:ml-4 '>
+        <p className='md:ml-4 text-md md:text-sm'>
           From the first pitch to IPO
         </p>
-        <div className='flex gap-1 mt-[.5vw] md:mt-0 md:ml-9 text-sm'>
+        <div className='flex gap-1 mt-[.5vw] md:mt-0 md:ml-9 text-md md:text-sm'>
           <div className='uppercase px-3 py-1 border-2 rounded-full'>
             start the project
           </div>
-          <div className='border-2 rounded-full flex items-center px-2'>
+          <div className='border-2 rounded-full flex items-center px-2 w-10 justify-center'>
             <span className='rotate-[45deg]'>
               <FaLongArrowAltUp />
             </span>
